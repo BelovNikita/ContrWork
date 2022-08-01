db2afcd78ec3b4d76066a9ef56849bbe37877866
BelovNikita <106664410+BelovNikita@users.noreply.github.com>
2022-08-01 22:53:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string s = Console.ReadLine();
+void Print(string[] arr)    
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+string[] a = s.Split(',');
